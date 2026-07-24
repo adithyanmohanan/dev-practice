ea50ec11343a04ab928b84e189f9aaa1c737fde2
adithyanmohanan <mahileo837@gmail.com>
2026-07-24 13:46:54 +0530

    Day 1 - varaiables and user input

---
+++ day1.py
@@ -0,0 +1,16 @@
+# variables
+name = "Adithyan"
+age = 22
+height = 6.1
+is_developer = True
+
+# Print them
+print(name)
+print(age)
+print(height)
+print(is_developer)
+
+# User input
+your_name= input("what is your name?")
+your_age= input("how old are you?")
+print("hello" + your_name + "! You are" + your_age + " years old.")
