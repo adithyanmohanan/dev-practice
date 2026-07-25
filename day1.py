7078c0576612ff82c422ca52d58dbd447d258e83
adithyanmohanan <mahileo837@gmail.com>
2026-07-25 12:37:19 +0530

    Day 1 complete - loops, lists, functions

--- day1.py
+++ day1.py
@@ -21,3 +21,28 @@ if age >= 18:
     print("You are an adult")
 else:
     print("You are a minor")
+
+#Loops
+for i in range(5):
+    print("Number:", i)
+
+#Lists
+fruits = ["apple", "banana", "mango", "orange"]
+print(fruits)
+print(fruits[0])
+print(fruits[2])
+
+for fruit in fruits:
+    print(" I Like", fruit)
+
+# Functions
+def greet(name):
+    print("Hello", name + "! Welcome to python.")
+
+def add_numbers(a, b):
+    result = a + b
+    print(a, "+", b, "=", result)
+
+greet("Adithyan")
+add_numbers(10, 5)
+add_numbers(100, 200)
