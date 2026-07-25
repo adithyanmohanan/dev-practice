79743807b684bf35a36de18a137053a3cad3671e
adithyanmohanan <mahileo837@gmail.com>
2026-07-25 23:12:12 +0530

    Day 2 - OOP classes and objects

--- day2.py
+++ day2.py
@@ -16,3 +16,28 @@ student["city"] = "Bangalore"
 #Loop through dictionary
 for key, value in student.items():
     print(key, ":", value)
+
+#OOP - Classes and Objects
+class Student:
+    def __init__(self, name, age, course):
+        self.name = name
+        self.age = age
+        self.course = course
+
+    def introduce(self):
+        print("Hi, I am", self.name, "studying", self.course)
+
+    def is_adult(self):
+        if self.age >= 18:
+            print(self.name, "is an adult")
+        else:
+            print(self.name, "is a minor")
+
+# Create objects
+student1 = Student("Adithyan", 22, "BCA")
+student2 = Student("Rahul", 17, "BSc")
+
+student1.introduce()
+student2.introduce()
+student1.is_adult()
+student2.is_adult()
