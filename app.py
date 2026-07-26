0d90d4f46810007f340415ef6ff9aa5f275ada98
adithyanmohanan <mahileo837@gmail.com>
2026-07-26 14:03:47 +0530

    Day 3 - JSON API with Flask

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app. route("/")
+@app.route("/")
 def home():
     return "Hello, World! My first Flask API!"
 
 @app.route("/about")
 def about():
     return "This is Adithyan's backend API"
+
+@app.route("/users")
+def get_users():
+    users = [
+        {"id": 1, "name": "Adithyan", "role": "developer"},
+        {"id": 2, "name": "Rahul", "role": "designer"},
+        {"id": 3, "name": "Priya", "role": "manager"}
+    ]
+    return jsonify(users)
 
 if __name__ == "__main__":
     app.run(debug=True)
